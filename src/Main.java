edcc1a4d85bc7f4269fd6446bdd00930262b9974
peterlewis19 <19lewis_p@bexleygs.co.uk>
2025-10-09 10:15:30 +0100

    Can't access files from outside the IDE, need to look at

--- src/Main.java
+++ src/Main.java
@@ -5,8 +5,8 @@ public class Main {
         //ArrayList<Node> unconnectedMapOfCoords = FileHandler.readNodesFromFile("\\\\BEX-FILE-01\\studenthome$\\19\\19lewis_p\\COMP SCI IA\\mapOfCoords500.txt");
         //int[][] edgeRelationMatrix = FileHandler.readMatrixFromFile("\\\\BEX-FILE-01\\studenthome$\\19\\19lewis_p\\COMP SCI IA\\adjacencyMatrix500.txt", unconnectedMapOfCoords.size());
 
-        ArrayList<Node> unconnectedMapOfCoords = FileHandler.readNodesFromFile("C:\\Users\\Peter Lewis\\Code\\IA Research\\route-planner-FINALFINAL\\500mapOfCoords.txt");
-        int[][] edgeRelationMatrix = FileHandler.readMatrixFromFile("C:\\Users\\Peter Lewis\\Code\\IA Research\\route-planner-FINALFINAL\\500edgeRelations.txt", unconnectedMapOfCoords.size());
+        ArrayList<Node> unconnectedMapOfCoords = FileHandler.readNodesFromFile("500mapOfCoords.txt");
+        int[][] edgeRelationMatrix = FileHandler.readMatrixFromFile("500edgeRelations.txt", unconnectedMapOfCoords.size());
 
         GUI graphics = new GUI(unconnectedMapOfCoords, edgeRelationMatrix);
     }
